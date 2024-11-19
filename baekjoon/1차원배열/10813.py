17e5817e7dfd3f1c35e16e5d87196cca3d0f0ca5
ctensoreo <ctensoreo@gmail.com>
2024-11-19 15:18:44 +0900

    feat: revert test

---
+++ baekjoon/1차원배열/10813.py
@@ -0,0 +1,18 @@
+A, B = map(int, input().split())
+
+basket_num_list = []
+for i in range(A):
+    basket_num_list.append(i+1)
+
+
+for j in range(B):
+    C, D = map(int, input().split())
+    a = basket_num_list[C-1]
+    b = basket_num_list[D-1]
+    basket_num_list[C-1] = b
+    basket_num_list[D-1] = a
+
+for k in range(A):
+    print(basket_num_list[k], end=' ')
+
+# print(" ".join(map(str, basket_num_list)))
