22f0d477cf5b0d06672b1368ac98f56d90a71e4b
ctensoreo <ctensoreo@gmail.com>
2024-11-19 12:51:55 +0900

    feat: 10871 완료

---
+++ baekjoon/1차원배열/10871.py
@@ -0,0 +1,24 @@
+A, B = map(int, input().split())
+
+N_list = list(map(int,input().split()))
+
+C = ""
+
+for i in range(A):
+    if B > N_list[i]:
+        C+=str(N_list[i])+" "
+
+# i=0
+# while i<A:
+#     if B > N_list[i]:
+#         print(N_list[i],end=" ") 
+#         # C+=str(N_list[i])+" "+"A" 
+#         # print(C)   
+#     i+=1 
+
+
+print(C)
+
+#새롭게 알게된 사실
+# print(N_list[i],end=" ") 이런식으로 작성하면 기본 옵션인 \n 이 되지 않고 공백만 생김 
+
