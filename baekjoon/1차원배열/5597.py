f2b3890a93a5dc058e9ba4ab139ed4c430967244
ctensoreo <ctensoreo@gmail.com>
2024-11-19 16:38:16 +0900

    feat: 풀이 완료(더 깔끔, 맘에듦)

--- baekjoon/1차원배열/5597.py
+++ baekjoon/1차원배열/5597.py
@@ -1,4 +1,13 @@
 all_regi_list = [i for i in range(1, 31)]
+
+#두번째
+for i in range(28):
+    A = int(input())
+    all_regi_list.remove(A)
+
+print(all_regi_list[0])
+print(all_regi_list[1])
+#첫번째
 second_list = [i for i in range(1, 31)]
 
 present_list = []
@@ -17,3 +26,4 @@ i=0
 while i<2:
     print(second_list[i])
     i+=1
+
