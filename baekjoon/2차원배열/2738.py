65cfeb6d531cb75754ee7218cc8924ffa73b3f90
ctensoreo <ctensoreo@gmail.com>
2024-12-17 16:53:02 +0900

    feat: 다른 방법으로 복습 완료

--- baekjoon/2차원배열/2738.py
+++ baekjoon/2차원배열/2738.py
@@ -31,3 +31,34 @@ for a in range(len(result_mat)):
 # print(A_mat)
 # print(B_mat)
 # print(result_mat)
+
+#복습
+# N, M = map(int, input().split())
+
+# A_mat_2 = []
+# B_mat_2 = []
+
+# b_result_mat = []
+# r_result_mat = []
+
+# for i in range(N):
+#     A = list(map(int, input().split()))
+#     A_mat_2.append(A)
+
+# for j in range(N):
+#     B = list(map(int, input().split()))
+#     B_mat_2.append(B)
+
+# #더해주면서 출력도 가능하겠네
+# for k in range(N):
+#     for l in range(M):
+#     #     print(A_mat_2[k][l] + B_mat_2[k][l], end=' ')
+#     # print()
+#         b_result_mat.append(A_mat_2[k][l] + B_mat_2[k][l])
+#     r_result_mat.append(b_result_mat)
+#     b_result_mat = []
+
+# for m in range(len(r_result_mat)):
+#     print(' '.join(map(str,r_result_mat[m])))
+
+
