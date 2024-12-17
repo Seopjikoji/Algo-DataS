a2be300f9683a201af9a411fee564489b698505c
ctensoreo <ctensoreo@gmail.com>
2024-12-17 16:39:55 +0900

    chore: 다 주석처리함, 원래 코드 주석 풀어놓음

--- baekjoon/1차원배열/10871.py
+++ baekjoon/1차원배열/10871.py
@@ -1,23 +1,23 @@
-# A, B = map(int, input().split())
+A, B = map(int, input().split())
 
-# N_list = list(map(int,input().split()))
+N_list = list(map(int,input().split()))
 
-# C = ""
+C = ""
 
-# for i in range(A):
+for i in range(A):
+    if B > N_list[i]:
+        C+=str(N_list[i])+" "
+
+# i=0
+# while i<A:
 #     if B > N_list[i]:
-#         C+=str(N_list[i])+" "
-
-# # i=0
-# # while i<A:
-# #     if B > N_list[i]:
-# #         print(N_list[i],end=" ") 
-# #         # C+=str(N_list[i])+" "+"A" 
-# #         # print(C)   
-# #     i+=1 
+#         print(N_list[i],end=" ") 
+#         # C+=str(N_list[i])+" "+"A" 
+#         # print(C)   
+#     i+=1 
 
 
-# print(C)
+print(C)
 
 #새롭게 알게된 사실
 # print(N_list[i],end=" ") 이런식으로 작성하면 기본 옵션인 \n 이 되지 않고 공백만 생김 
