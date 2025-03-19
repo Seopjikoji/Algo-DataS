7265ad8c5d2beee5f4c9a134a4b40506533f2c95
ctensoreo <ctensoreo@gmail.com>
2025-03-19 16:38:29 +0900

    feat: 10798 풀이 완료(보완 필요)

---
+++ baekjoon/2차원배열/10798.py
@@ -0,0 +1,15 @@
+words = []
+for i in range(5):
+  a = input() 
+  words.append(a)
+
+# words = [input() for i in range(5)]
+
+for i in range(15):
+  for j in range(5):
+    if len(words[j]) > i: 
+        print(words[j][i], end='')
+
+
+
+
