2ef850981feebc0789724f2d4e23aabed9a3b242
ctensoreo <ctensoreo@gmail.com>
2026-08-03 15:15:17 +0900

    feat: 유연근무제 풀이 완료(미구현 부분 보완)

--- programmers/4.flexible-work-system.py
+++ programmers/4.flexible-work-system.py
@@ -1,14 +1,29 @@
+def cal_accepted_schedule_time(schedule):
+    hour = schedule // 100
+    minute = schedule % 100
+
+    if minute + 10 >= 60:
+        hour += 1
+        minute = (minute + 10) % 60
+    else:
+        minute += 10
+    
+    return hour * 100 + minute
+    
+
 def solution(schedules, timelogs, startday):
     answer = 0
 
     for i in range(len(schedules)):
       indpasscount = 0
       fixedday = startday
+      a = cal_accepted_schedule_time(schedules[i])
+      print(a)
       for j in range(7):
         
         # print(startday, timelogs[i][j], schedules[i] + 10)
         if startday != 6 and startday != 7:
-          if timelogs[i][j] <= schedules[i] + 10:
+          if timelogs[i][j] <= cal_accepted_schedule_time(schedules[i]):
             indpasscount += 1
         
         # print(startday, i, 'S')
@@ -17,16 +32,16 @@ def solution(schedules, timelogs, startday):
           startday = 1
 
       startday = fixedday
-      print(startday, 'S')
-      # print(indpasscount, 'I')        
+      # print(startday, 'S')
+      print(indpasscount, 'I')        
       if indpasscount == 5:
         answer += 1 
 
     return answer
 
-schedules = [700, 800, 1100]
-timelogs = [[710, 2359, 1050, 700, 650, 631, 659], [800, 801, 805, 800, 759, 810, 809], [1105, 1001, 1002, 600, 1059, 1001, 1100]]
-startday = 5
+schedules = [730, 855, 700, 720]
+timelogs = [[710, 700, 650, 735, 700, 931, 912], [908, 901, 805, 815, 800, 831, 835], [705, 701, 702, 705, 710, 710, 711], [707, 731, 859, 913, 934, 931, 905]]
+startday = 1
 
 a = solution(schedules, timelogs, startday)
 print(a)
