9f14bf7bef8067519cf77cc15e36208f237c08da
ctensoreo <ctensoreo@gmail.com>
2026-08-15 05:46:34 +0900

    feat: 코드 보완

--- improve/14.p.py
+++ improve/14.p.py
@@ -7,38 +7,47 @@ def find_start_index(park, h, w):
 def solution(park, routes):
     h, w = len(park), len(park[0])
 
-    x = 0
-    y = 0
-
     direction_index_dict = { 'E': 0, 'W': 1, 'S': 2, 'N': 3 }
+
+    y = 0
+    x = 0
 
     dy = [0, 0, 1, -1]
     dx = [1, -1, 0, 0]
 
+    y, x = find_start_index(park, h, w)
 
+    print(y, x, '시작 위치')
     for route in routes:
         direction, delta = route.split()
         delta = int(delta)
         direction_index = direction_index_dict[direction]
+        print(direction_index, 'index', direction, 'direction')
+        
+        ny, nx = y, x
 
-        ny, nx = find_start_index(park, h, w)
+        is_pass = True
 
         for _ in range(delta):
+            print('delta', delta)
             # 시작점에서 수행
             ny = ny + dy[direction_index]
             nx = nx + dx[direction_index]
-
+            print(ny, nx, '현재 위치')
             # 조건 안맞으면 해당 delta 값 순차 진행하다가 이탈하면 됨
             if ny < 0 or ny >= h or nx < 0 or nx >= w:
+                is_pass = False
                 break
 
             # X 인지 체크      
             if park[ny][nx] == 'X':
+                is_pass = False
                 break
 
+        if is_pass:    
             y = ny
             x = nx
-
+        print(y, x, '패스 여부에 따른 delta 이동 후 위치')
     return [y, x]        
 
     # print(ny, nx)
