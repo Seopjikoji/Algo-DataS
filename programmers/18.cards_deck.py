fde8dc7d28a28552128df8bdb479b4e065d03b3a
ctensoreo <ctensoreo@gmail.com>
2026-08-20 14:13:45 +0900

    feat: 카드 뭉치 코드 개선 완료

--- programmers/18.cards_deck.py
+++ programmers/18.cards_deck.py
@@ -4,19 +4,19 @@ def solution(cards1, cards2, goal):
     cards1_index = 0
     cards2_index = 0
 
+    # 카드 뭉치의 각각 길이
+    card1_len = len(cards1)
+    card2_len = len(cards2)
+
     # goal 순회하면서 만들 수 있는 단어인가 체크, 카드 덱 순서대로 이동
     for g in goal:
-        if cards1[cards1_index] == g:
-            # print(cards1_index, '1')
-            if cards1_index < len(cards1) - 1:
-                cards1_index += 1
+        if cards1_index < card1_len and cards1[cards1_index] == g:
+            cards1_index += 1
             
-        elif cards2[cards2_index] == g:
-            if cards2_index < len(cards2) - 1 :
-                cards2_index += 1
+        elif cards2_index < card2_len and cards2[cards2_index] == g:
+            cards2_index += 1
         else:
             return 'No'
-
     return 'Yes'    
         
 
