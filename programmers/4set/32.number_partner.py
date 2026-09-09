3e63189fc4520887eb0133dddd21dc295e6c3254
ctensoreo <ctensoreo@gmail.com>
2026-09-09 16:32:03 +0900

    feat: 숫자 짝꿍 문제 풀이 완료(초기 아이디어는 시간 초과)

---
+++ programmers/4set/32.number_partner.py
@@ -0,0 +1,55 @@
+def solution(X, Y):
+
+    # 순회하면서 X 내 숫자 각각의 개수를 기록하는 dict 를 만든다
+    count = {}
+
+    # 겹치는 결과 list
+    result = [] 
+
+    for x in X:
+        if x in count:
+            count[x] += 1
+        else:
+            count[x] = 1
+
+    # print(count)
+
+    for y in Y:
+        if y in count and count[y] > 0:
+            result.append(y)
+            count[y] -= 1
+
+    result = sorted(result, reverse=True)
+
+    if len(result) != 0:
+        if result[0] == '0':
+            return '0'
+        else:
+            return ''.join(result)
+    else:
+        return '-1'
+
+    
+    # 시간 초과
+    # # 둘이 일치하는 숫자 담은 리스트
+    # result = []
+
+    # # X 기준으로 순회하면서 일치하는
+    # for target in X:
+
+    #     i = Y.find(target)
+    #     if i != -1:
+    #         Y = Y[:i] + Y[i+1:]
+    #         # print(Y)
+    #         result.append(target)
+
+    # result = sorted(result, reverse=True)
+
+    # if len(result):
+    #     return str(int(''.join(result)))
+    # else:
+    #     return '-1'
+
+
+a = solution('100', '203045')
+print(a)
