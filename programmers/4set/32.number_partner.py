72751ee39bb0687bd87f43570bde386be9155b49
ctensoreo <ctensoreo@gmail.com>
2026-09-09 17:09:54 +0900

    feat: 라이브러리 활용하기

--- programmers/4set/32.number_partner.py
+++ programmers/4set/32.number_partner.py
@@ -1,33 +1,47 @@
+from collections import Counter
+
 def solution(X, Y):
 
-    # 순회하면서 X 내 숫자 각각의 개수를 기록하는 dict 를 만든다
-    count = {}
-
-    # 겹치는 결과 list
-    result = [] 
-
-    for x in X:
-        if x in count:
-            count[x] += 1
-        else:
-            count[x] = 1
-
-    # print(count)
-
-    for y in Y:
-        if y in count and count[y] > 0:
-            result.append(y)
-            count[y] -= 1
-
-    result = sorted(result, reverse=True)
-
-    if len(result) != 0:
+    # 라이브러리 활용하기
+    count = Counter(X) & Counter(Y)
+    
+    result = ''.join(sorted(count.elements(), reverse=True))
+    if len(result):
         if result[0] == '0':
             return '0'
         else:
-            return ''.join(result)
+            return result
     else:
         return '-1'
+    
+    # # 순회하면서 X 내 숫자 각각의 개수를 기록하는 dict 를 만든다
+    # count = {}
+
+    # # 겹치는 결과 list
+    # result = [] 
+
+    # for x in X:
+    #     if x in count:
+    #         count[x] += 1
+    #     else:
+    #         count[x] = 1
+
+    # # print(count)
+
+    # for y in Y:
+    #     if y in count and count[y] > 0:
+    #         result.append(y)
+    #         count[y] -= 1
+
+    # result = sorted(result, reverse=True)
+
+    # if len(result) != 0:
+    #     if result[0] == '0':
+    #         return '0'
+    #     else:
+    #         return ''.join(result)
+    # else:
+    #     return '-1'
 
     
     # 시간 초과
@@ -51,5 +65,5 @@ def solution(X, Y):
     #     return '-1'
 
 
-a = solution('100', '203045')
+a = solution('100', '2345')
 print(a)
