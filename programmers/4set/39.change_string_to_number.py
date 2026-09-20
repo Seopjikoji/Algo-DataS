29bc87c76923bf5c61229e20261c864812ef0b92
ctensoreo <ctensoreo@gmail.com>
2026-09-21 07:55:03 +0900

    chore: 함수 기능 부연 설명 추가(주석)

--- programmers/4set/39.change_string_to_number.py
+++ programmers/4set/39.change_string_to_number.py
@@ -3,7 +3,7 @@ def solution(s):
     # 숫자 사전
     number_dict = { 'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 }
 
-    # 숫자 사전 순회하면서 있는지 확인해서 replace
+    # 숫자 사전 순회하면서 있는지 확인해서 replace(string > string 으로만 가능)
     for i in number_dict:
         if i in s:
             s = s.replace(i, str(number_dict[i]))
